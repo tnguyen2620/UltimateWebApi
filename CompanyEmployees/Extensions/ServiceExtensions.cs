a49d6346c93fcb59d2044c7fb702d5a3c12f4a90
tnguyen2620 <jamesdoam@gmail.com>
2023-01-02 00:13:47 +1100

    2.2 & 2.3 install Nlog, implement LoggerManager and nlog.config

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -8,16 +8,13 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureCors(this IServiceCollection services) =>
              services.AddCors(options =>
              {
-                 options.AddPolicy("CorsPolicy", builder =>
-                 builder.AllowAnyOrigin()
+                 options.AddPolicy("CorsPolicy", builder => builder.AllowAnyOrigin()
                  .AllowAnyMethod()
                  .AllowAnyHeader());
              });
         public static void ConfigureIISIntegration(this IServiceCollection services) =>
              services.Configure<IISOptions>(options =>
             {
-            }
-
-
+            });
  }
 }
