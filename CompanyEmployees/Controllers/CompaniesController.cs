9ad0b2b2754e7ec2094ba02d90c571a04a4e4bc3
tnguyen2620 <jamesdoam@gmail.com>
2023-01-02 16:39:34 +1100

    4.1 Create an employ Company API Controller

---
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+    }
+}
