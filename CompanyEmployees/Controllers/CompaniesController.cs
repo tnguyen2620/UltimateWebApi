2c31952bfd152a99bb7ff1e10c501b8f96e48804
tnguyen2620 <jamesdoam@gmail.com>
2023-01-02 17:11:19 +1100

    4.6 Using AutoMapper to return companyDto objects

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -1,9 +1,12 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Entities.Dto;
 using LoggerService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CompanyEmployees.Controllers
@@ -14,11 +17,13 @@ namespace CompanyEmployees.Controllers
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
 
-        public CompaniesController(IRepositoryManager repository, ILoggerManager logger)
+        public CompaniesController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
         [HttpGet]
         public IActionResult GetCompanies()
@@ -26,12 +31,7 @@ namespace CompanyEmployees.Controllers
             try
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges: false);
-                var companiesDto = companies.Select(c => new CompanyDto
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    FullAddress = string.Join(' ', c.Address, c.Country)
-                }).ToList();
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
                 return Ok(companiesDto);
             }
--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -30,6 +30,7 @@ namespace CompanyEmployees
             services.ConfigureLoggerService();
             services.ConfigureSqlContext(Configuration);
             services.ConfigureRepositoryManager();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
         }
 
