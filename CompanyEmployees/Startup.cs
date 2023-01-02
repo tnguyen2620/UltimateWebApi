606fa9d13b68b695deaf76d5b37ae24eda51a912
tnguyen2620 <jamesdoam@gmail.com>
2023-01-02 17:21:23 +1100

    5.2 Startup configration & clean the company controller

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -1,4 +1,5 @@
 using CompanyEmployees.Extensions;
+using LoggerService;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,7 +36,7 @@ namespace CompanyEmployees
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
@@ -45,7 +46,7 @@ namespace CompanyEmployees
             {
                 app.UseHsts();
             }
-
+            app.ConfigureExceptionHandler(logger);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCors("CorsPolicy"); //Add CORS configuration to app pipeline
