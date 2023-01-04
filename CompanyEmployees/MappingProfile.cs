c574d95b4d5f82f70507038ab76c9ccc6536db78
tnguyen2620 <jamesdoam@gmail.com>
2023-01-04 23:54:34 +1100

    11.1 Update an employee from a company

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -11,10 +11,11 @@ namespace CompanyEmployees
             CreateMap<Company, CompanyDto>()
                .ForMember(c => c.FullAddress,
                 opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
-
-            CreateMap<Employee, EmployeeDto>();
             CreateMap<CompanyForCreationDto, Company>();
+
+            CreateMap<Employee, EmployeeDto>();            
             CreateMap<EmployeeForCreationDto, Employee>();
+            CreateMap<EmployeeForUpdateDto, Employee>();
         }
     }
 }
