2d34863c0d8a6e5df6dc8dfd5fcd9e162d3a0046
tnguyen2620 <jamesdoam@gmail.com>
2023-01-04 23:15:23 +1100

    9.5 Create a collection of companies

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -21,6 +21,8 @@ namespace Repository
             FindByCondition(c => c.Id.Equals(companyId), trackChanges).SingleOrDefault();
 
         public void CreateCompany(Company company) => Create(company);
+        public IEnumerable<Company> GetByIds(IEnumerable<Guid> ids, bool trackChanges) =>
+            FindByCondition(x => ids.Contains(x.Id), trackChanges).ToList();
 
     }
 }
