21a88c15f76cd643ff84ed0bd82011316844029d
tnguyen2620 <jamesdoam@gmail.com>
2023-01-05 09:30:04 +1100

    13.1 Validate Employee's age

--- Entities/Dto/EmployeeForCreationDto.cs
+++ Entities/Dto/EmployeeForCreationDto.cs
@@ -12,7 +12,7 @@ namespace Entities.Dto
         [Required(ErrorMessage = " Employee name is required.")]
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Age is a required field.")]
+        [Range(18, 100, ErrorMessage = "Age is required and it can't be lower than 18")]
         public int Age { get; set; }
         [Required(ErrorMessage = "Position is a required field.")]
         [MaxLength(20, ErrorMessage = "Maximum length for the Position is 20 characters.")]
