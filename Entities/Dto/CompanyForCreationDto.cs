af4c6b6675d12ef707df7d5151cbb57065a6cbdc
tnguyen2620 <jamesdoam@gmail.com>
2023-01-04 23:08:31 +1100

    9.4 Create company with multiple employees (parent with children)

--- Entities/Dto/CompanyForCreationDto.cs
+++ Entities/Dto/CompanyForCreationDto.cs
@@ -11,5 +11,7 @@ namespace Entities.Dto
         public string Name { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+        public IEnumerable<EmployeeForCreationDto> Employees { get; set; }
+
     }
 }
