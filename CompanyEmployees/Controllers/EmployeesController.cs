51b3d3a4af759c38f5e2c388d059324ebe193e60
tnguyen2620 <jamesdoam@gmail.com>
2023-01-07 23:43:24 +1100

    16.4 Api paging with PagedList implementation

--- CompanyEmployees/Controllers/EmployeesController.cs
+++ CompanyEmployees/Controllers/EmployeesController.cs
@@ -8,6 +8,7 @@ using LoggerService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -39,6 +40,7 @@ namespace CompanyEmployees.Controllers
                 return NotFound();
             }
             var employeesFromDb = await _repository.Employee.GetEmployeesAsync(companyId, employeeParameters, trackChanges: false);
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(employeesFromDb.MetaData));
             var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
             return Ok(employeesDto);
         }
