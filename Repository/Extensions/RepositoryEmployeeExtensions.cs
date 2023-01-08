07b15d759380b1ce657c04de097090d5a791b396
tnguyen2620 <jamesdoam@gmail.com>
2023-01-09 10:18:31 +1100

    19.5 Improve the sorting functionality

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using Repository.Extensions.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,25 +25,10 @@ namespace Repository.Extensions
         {
             if (string.IsNullOrWhiteSpace(orderByQueryString))
                 return employees.OrderBy(e => e.Name);
-            var orderParams = orderByQueryString.Trim().Split(',');
-            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var orderQueryBuilder = new StringBuilder();
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrWhiteSpace(param))
-                    continue;
-                var propertyFromQueryName = param.Split(" ")[0];
-                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
-                if (objectProperty == null)
-                    continue;
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
-            }
-            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
             if (string.IsNullOrWhiteSpace(orderQuery))
                 return employees.OrderBy(e => e.Name);
-            var employeesToBeReturned = employees.OrderBy(orderQuery);
-            return employeesToBeReturned;
+            return employees.OrderBy(orderQuery);
         }
     }
 }
