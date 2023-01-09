ddcb33fea43b97e747274d9a6550b26672d3b97d
tnguyen2620 <jamesdoam@gmail.com>
2023-01-09 14:32:25 +1100

    20.4.1 Really resolving XML serialization problems by replacing ExpandoObject by Entity

--- Repository/DataShaping/DataShaper.cs
+++ Repository/DataShaping/DataShaper.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -16,12 +17,12 @@ namespace Repository.DataShaping
         {
             Properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
         }
-        public IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string fieldsString)
+        public IEnumerable<Entity> ShapeData(IEnumerable<T> entities, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
             return FetchData(entities, requiredProperties);
         }
-        public ExpandoObject ShapeData(T entity, string fieldsString)
+        public Entity ShapeData(T entity, string fieldsString)
         {
             var requiredProperties = GetRequiredProperties(fieldsString);
             return FetchDataForEntity(entity, requiredProperties);
@@ -46,9 +47,9 @@ namespace Repository.DataShaping
             }
             return requiredProperties;
         }
-        private IEnumerable<ExpandoObject> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
+        private IEnumerable<Entity> FetchData(IEnumerable<T> entities, IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedData = new List<ExpandoObject>();
+            var shapedData = new List<Entity>();
             foreach (var entity in entities)
             {
                 var shapedObject = FetchDataForEntity(entity, requiredProperties);
@@ -57,9 +58,9 @@ namespace Repository.DataShaping
 
             return shapedData;
         }
-        private ExpandoObject FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
+        private Entity FetchDataForEntity(T entity, IEnumerable<PropertyInfo> requiredProperties)
         {
-            var shapedObject = new ExpandoObject();
+            var shapedObject = new Entity();
             foreach (var property in requiredProperties)
             {
                 var objectPropertyValue = property.GetValue(entity);
