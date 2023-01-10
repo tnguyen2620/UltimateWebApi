2144023a8d3e85fedf944f0785196c6d0e68d797
tnguyen2620 <jamesdoam@gmail.com>
2023-01-10 21:19:11 +1100

    24.2 Company Controller v2

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
 
 namespace CompanyEmployees.Controllers
 {
+    [ApiVersion("1.0")]
     [Route("api/companies")]
     [ApiController]
     public class CompaniesController : ControllerBase
